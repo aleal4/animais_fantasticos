72e8f8dac2944eb37121958d20d6a40f11b007e7
Augusto Leal <aleal4@gmail.com>
2022-03-11 19:09:32 -0800

    mobile

--- js/modules/dropdown-menu.js
+++ js/modules/dropdown-menu.js
@@ -1,7 +1,7 @@
 import outsideClick from "./outsideclick.js";
 
 export default function initDropdownMenu() {
-   const dropdownMenus = document.querySelectorAll("[data-dropdown");
+  const dropdownMenus = document.querySelectorAll("[data-dropdown");
 
   dropdownMenus.forEach((menu) => {
     ["touchstart", "click"].forEach((userEvent) => {
